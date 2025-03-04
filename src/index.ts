8dc3a8e3a5a88b2d93d934e12086a7dd057d3408
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2025-03-04 08:57:51 +0100

    Bump version to 1.1.0 and enhance user role attribute injection with title tracking

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,45 @@
 import type { HookConfig } from '@directus/extensions';
 
 const hook: HookConfig = ({ embed }) => {
-  embed(
-    'body',
+	embed(
+		'body',
     /* html */ `
-		<script>
-			function injectAttribute(name, value) {
-				if (!value) return;
-				const sanitizedValue = value.toLowerCase().replace(/\\s/g, '-');
-				document.body.setAttribute(name, sanitizedValue);
-			}
-			(async () => {
-				try {
-					const response = await fetch(window.location.origin + '/users/me?fields=id,role.name').then((res) => res.json());
-					injectAttribute('data-user-id', response?.data?.id);
-					injectAttribute('data-user-role', response?.data?.role?.name);
-				} catch (error) {
-					console.error('[current-role-hook]', error.message);
-				}
-			})();
-		</script>
-		`
-  );
+    <script>
+      function injectAttribute(name, value) {
+        if (!value) return;
+        const sanitizedValue = value.toLowerCase().replace(/\\s/g, '-');
+        document.body.setAttribute(name, sanitizedValue);
+      }
+
+      async function fetchAndInjectAttributes() {
+        try {
+          const response = await fetch(\`\${window.location.origin}/users/me?fields=id,role.name\`).then(res => res.json());
+          injectAttribute('data-user-id', response?.data?.id);
+          injectAttribute('data-user-role', response?.data?.role?.name ?? 'public');
+        } catch (error) {
+          console.error('[current-role-hook]', error.message);
+        }
+      }
+
+      (async () => {
+		// Initial attributes
+		injectAttribute('data-user-role', 'public');
+
+        // Initial fetch
+        await fetchAndInjectAttributes();
+
+        // Track changes to the document title, refetch if it changes
+        let oldDirectusTitle = document.title;
+        setInterval(async () => {
+          if (document.title !== oldDirectusTitle) {
+            oldTitle = document.title;
+            await fetchAndInjectAttributes();
+          }
+        }, 1000);
+      })();
+    </script>
+    `
+	);
 };
 
 export default hook;
